24b49edc4af0a8cc8ec29afc5ad740c03b410cd7
S N <snam@zoox.com>
2026-03-15 17:01:19 -0700

    added asset and animation

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,22 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Player;
+mod player;
+
+use crate::player::PlayerPlugin;
 
 fn main() {
   App::new()
-    .add_plugins(DefaultPlugins)
-    .add_systems(Startup, setup)
-    .add_systems(Update, move_player)
+    .insert_resource(ClearColor(Color::WHITE))
+    .add_plugins(DefaultPlugins.set(AssetPlugin {
+      // assets live in src/assets
+      file_path: "src/assets".into(),
+      ..default()
+    }))
+    .add_systems(Startup, setup_camera)
+    .add_plugins(PlayerPlugin)
     .run();
 }
 
-fn setup(mut commands: Commands) {
+fn setup_camera(mut commands: Commands) {
   commands.spawn(Camera2d);
-
-  commands.spawn((
-    Text2d::new("@"),
-    TextFont {
-      font_size: 12.0,
-      font: default(),
-      ..default()
-    },
-    TextColor(Color::WHITE),
-    Transform::from_translation(Vec3::ZERO),
-    Player,
-  ));
-}
-
-fn move_player(
-  input: Res<ButtonInput<KeyCode>>,
-  time: Res<Time>,
-  mut player_transform: Single<&mut Transform, With<Player>>,
-) {
-  let mut direction = Vec2::ZERO;
-  if input.pressed(KeyCode::ArrowLeft) {
-    direction.x -= 1.0;
-  }
-  if input.pressed(KeyCode::ArrowRight) {
-    direction.x += 1.0;
-  }
-  if input.pressed(KeyCode::ArrowUp) {
-    direction.y += 1.0;
-  }
-  if input.pressed(KeyCode::ArrowDown) {
-    direction.y -= 1.0;
-  }
-  if direction != Vec2::ZERO {
-    let speed: f32 = 300.0; // pixels per second
-    let delta = direction.normalize() * speed * time.delta_secs();
-    player_transform.translation.x += delta.x;
-    player_transform.translation.y += delta.y;
-  }
 }
